2b03d58c60db6d65012f879c7d66e9c24508fda3
Jhonatan Tipan <jhonatan.tipan@udla.edu.ec>
2023-12-06 15:19:40 -0500

    Imprimir la nota de los alumnos de los 3 progresos

--- Tipan_EvaluacionP2.c
+++ Tipan_EvaluacionP2.c
@@ -15,7 +15,15 @@ int main() {
             calificaciones[i][j] = rand() % 11; // aleatoria entre 0 y 10
         }
     }
-
+    // Imprimir las notas de los alumnos
+    printf("Notas de los alumnos:\n");
+    for (int i = 0; i < alumnos; i++) {
+        printf("Alumno %d: ", i+1);
+        for (int j = 0; j <progresos; j++) {
+            printf("%d ", calificaciones[i][j]);
+        }
+        printf("\n");
+    }
 
 
     return 0;
